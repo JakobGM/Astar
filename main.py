cafa583d63823aacdb44a311f1948539afd6af17
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-10-22 19:11:44 +0200

    Add several modes for main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from sys import argv
+
 from mazesolver import MazeSolver
 
-if __name__ == '__main__':
+if argv[1] == 'task_1.2':
     board_paths = 'mazesolver/tests/boards/board-1-'
+
+    for board_number in [1, 2, 3, 4]:
+        with open(board_paths + str(board_number) + '.txt') as board_file:
+            maze_solver = MazeSolver(board_file.read())
+            maze_solver.visualize()
+
+if argv[1] == 'task_2.2':
+    board_paths = 'mazesolver/tests/boards/board-2-'
 
     for board_number in [1, 2, 3, 4]:
         with open(board_paths + str(board_number) + '.txt') as board_file:
